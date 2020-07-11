ce9e96045aed3ecb7abf19061599d5ba4d634a09
gazanfarali <gasyed@live.com>
2020-07-11 11:32:13 +0100

    adding docker-compose

---
+++ install.sh
@@ -0,0 +1,23 @@
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        # ...
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        # POSIX compatibility layer and Linux environment emulation for Windows
+elif [[ "$OSTYPE" == "msys" ]]; then
+        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+elif [[ "$OSTYPE" == "win32" ]]; then
+        # I'm not sure this can happen.
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        # ...
+else
+        # Unknown.
+		
+	
+fi
+
+# below need to be added to the script do move docker-compose to
+# /usr/local/bin/docker-compose
+#and make it executable "chmod +x docker-compose"
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
